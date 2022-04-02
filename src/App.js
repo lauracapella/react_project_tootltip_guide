eb855f5c081b5cc62947bbe7b1916232ac2a3c55
Laura Capella <laura@laguapacomunicacion.com>
2022-04-02 15:05:31 +0200

    style sentences

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 //import logo from './logo.svg';
 import './App.css';
 import data from './data'
+//import styled from 'styled-components';
 //import Frase from '../components/scene/Scene'
 //import React, { count, useState } from "react";
 
 
 
 function Frase(props){
+  
   //const frases =[];
   //let i =1;
   //const fraseShow = frases.map (frase => i++)
   //alert (frases[i]);
   //const [count, setCount] = useState(frases.i);
-  return <h3>{data.frases[props.i]}</h3>
+  return <h3 className='style__frase' >{data.frases[props.i]}</h3>
 }
 
 function App() {
