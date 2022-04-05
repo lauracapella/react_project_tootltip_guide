772fbbe378cd39c5d4067e54d20db34e407b06cd
Laura Capella <laura@laguapacomunicacion.com>
2022-04-05 18:01:58 +0200

    colored sentence loop

--- src/App.js
+++ src/App.js
@@ -8,14 +8,18 @@ function App(props) {
   const [count, setCount] = useState(0);
   const contadorMas = () => setCount(count + 1);
   const contadorMenos = () => setCount(count - 1);
+  const contadorInicio = () => setCount(0);
+  const contadorFinal = () => setCount(3);
+
+
   const frasesList = data.frases.map((frase, index) => 
-    <li key={index} className={index===count? 'style__frase_red': 'style__frase'}> {frase}</li> 
+    <li key={frase} className={(index===count) ? 'style__frase_red': 'style__frase'}> {frase}</li> 
   );
   
   return (
     <>
-    <button className='botones' onClick={contadorMenos}>Atras</button>
-    <button className='botones' onClick={contadorMas}>Siguiente</button>
+    <button className='botones' onClick={(count >= 0) ? contadorMenos : contadorFinal}>Atras</button>
+    <button className='botones' onClick={(count <= 3) ? contadorMas : contadorInicio}>Siguiente</button>
     <h3 ><ul>{frasesList}</ul></h3>
     </>
   );
