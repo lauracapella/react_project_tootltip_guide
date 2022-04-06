e489180f1e620ed2463b3d540d173e46f635fc29
Laura Capella <laura@laguapacomunicacion.com>
2022-04-06 12:39:03 +0200

    welcome page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { count, useState } from "react";
 import data from "./data"
 
-function App(props) {
+function Historia(props) {
   const [count, setCount] = useState(0);
   const contadorMas = () => setCount(count + 1);
   const contadorMenos = () => setCount(count - 1);
@@ -18,6 +18,36 @@ function App(props) {
     <button className='botones' onClick={(count >= 1) ? contadorMenos : contadorFinal}><h2>ENRERE</h2></button>
     <button className='botones' onClick={(count <= frasesList.length-2) ? contadorMas : contadorInicio}><h2>SEGÜENT</h2></button>
     <h3 ><ul>{frasesList}</ul></h3>
+    </div>
+  );
+}
+
+function Wellcome (props){
+  let mostrarPagina = true;
+
+  return (
+    <div class='welcome__wrap'>
+    <div class='welcome'>
+      <h3>Esta es la historia de un superheroe.</h3>
+    <button className='botones' onClick={props.goToHome}>EMPEZAR</button>
+    </div>
+    </div>
+  );
+}
+
+
+
+function App(props) {
+
+  const [mostrarPagina, setMostrarPagina] = useState(false)
+
+  const changePage = () => {
+    setMostrarPagina(true);
+  }
+
+  return (
+    <div>
+      {mostrarPagina ? <Historia/> : <Wellcome goToHome = {changePage} />}
     </div>
   );
 }
